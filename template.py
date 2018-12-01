2f96df2077033ada7df9d1788a2af266f8385aaa
Tim Garvin <tcgarvin@gmail.com>
2018-12-01 00:47:12 -0500

    Template tweaks

--- template.py
+++ template.py
@@ -5,14 +5,22 @@ def parse_line(line):
     return line
 
 def parse_input(lines):
-    return map(parse_line, lines)
+    return list(map(parse_line, lines))
 
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser()
     arg_parser.add_argument('file', metavar='FILE', nargs="?", default="-")
     args = arg_parser.parse_args()
+    puzzle_input = parse_input(fileinput.input(args.file))
 
-    parsed = parse_input(fileinput.input(args.file))
+    # Part 1
+    part1 = "Unknown"
 
-    print(list(parsed))
+    print("Part 1: " + str(part1))
+
+
+    # Part 2
+    part2 = "Unknown"
+
+    print("Part 2: " + str(part2))
